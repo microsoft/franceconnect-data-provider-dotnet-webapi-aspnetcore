1008ac825c21e3d71f88f258d9b333f456e9a3ec
Amine A <15179425+AmineI@users.noreply.github.com>
2021-08-06 21:16:35 +0200

    Create a migration to add seed data to the database (#5)

---
+++ Source/WebApi-Data-Provider-DotNet/Migrations/20210805194806_CreateSeedData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiDataProviderDotNet.Migrations
+{
+    public partial class CreateSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "ValueOne", "ValueTwo" },
+                values: new object[] { "4c56f180-c0db-4954-bae6-4f156b8f17cf", "wossewodda-3728@yopmail.com", "Valeur test 1", "Seconde valeur test" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "4c56f180-c0db-4954-bae6-4f156b8f17cf");
+        }
+    }
+}
