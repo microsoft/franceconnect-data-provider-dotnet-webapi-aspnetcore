eed9b9b6760e7124bbac6471cc8878006f17b17c
Amine A <15179425+AmineI@users.noreply.github.com>
2021-08-23 14:39:11 +0200

    Refresh License headers

--- Source/WebApi-Data-Provider-DotNet/Program.cs
+++ Source/WebApi-Data-Provider-DotNet/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace WebApi_Data_Provider_DotNet
