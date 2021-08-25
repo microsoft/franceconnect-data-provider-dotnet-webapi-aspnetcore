434a9e4cc69208cae1b8a29e22276643fa0366c5
Amine A <15179425+AmineI@users.noreply.github.com>
2021-08-25 15:23:34 +0200

    Documentation, code cleanup, home page licenses refresh (#11)
    * Add documentation
    
    Project development Quick Start, Azure deployment, and GitHub Actions
    
    * Refresh License headers
    
    * Add app launch settings and reorganize appsettings.json
    
    
    * Add a home page

---
+++ Source/WebApi-Data-Provider-DotNet/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Data_Provider_DotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
