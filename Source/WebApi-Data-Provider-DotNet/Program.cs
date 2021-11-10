3d69e1aa64c34891f3dde47dbac4ea6d663d80c2
Amine A <15179425+AmineI@users.noreply.github.com>
2021-11-10 15:46:01 +0100

    Migrate to .NET 6.0 (RC) (#12)
    * Update Github Action to use .NET 5
    
    Update build-and-deploy.yml
    
    * Migrate project files and dependencies to .NET 6.0 RC1
    
    * Migrate Program.cs to the new .NET 6.0 template format
    
    Consolidating Program.cs and Startup.cs in a single file
    
    * Update Github Actions to use .NET 6 prerelease
    
    * Update dotnet-ef tool to version 6 RC
    
    * Update README.md
    
    * Update Project references to .NET 6.0 (public release)
    
    * Update the EntityFramework CLI tool to 6.0 (final)
    
    * Update GitHub Actions to .NET 6 final
    
    Note : There is still a workaround in place to enable .NET 6 in the CodeQL GitHub Action.

--- Source/WebApi-Data-Provider-DotNet/Program.cs
+++ Source/WebApi-Data-Provider-DotNet/Program.cs
@@ -1,24 +1,68 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebApi_Data_Provider_DotNet.Models;
 
-namespace WebApi_Data_Provider_DotNet
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
+
+// Add database services.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    public class Program
+    if (builder.Environment.IsProduction())
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     }
+    else
+    {
+        // Suggested for development environments, as the database is thus hosted with the web app instead of a separate server.
+        options.UseInMemoryDatabase("InMemory");
+    }
+});
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//TODO Regions
+
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+
+if (app.Environment.IsProduction())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+    app.UseHttpsRedirection();
 }
+else
+{
+    app.UseBrowserLink();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseRequestLocalization(new RequestLocalizationOptions { DefaultRequestCulture = new RequestCulture("fr-FR") });
+
+app.UseCookiePolicy();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
